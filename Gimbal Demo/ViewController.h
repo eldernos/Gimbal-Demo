4b26cac2879821af40c300fdc0c6e989ba7d708f
Christopher Anderson <chanderson@xpclient01.rrad.priv>
2013-10-23 08:10:39 -0500

    Initial Commit

---
+++ Gimbal Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Gimbal Demo
+//
+//  Created by Christopher Anderson on 10/23/13.
+//  Copyright (c) 2013 GMR Marketing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Gimbal Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gimbal Demo
+//
+//  Created by Christopher Anderson on 10/23/13.
+//  Copyright (c) 2013 GMR Marketing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
